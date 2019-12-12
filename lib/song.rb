03f641f1e62b52ae84a23c3e62795dbe193e8c9c
Isaac Avilez <isaac1180@aol.com>
2019-12-12 21:24:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,10 +23,10 @@ class Song
     @@artists
   end
   
-#   def self.genres 
-#     @@genres 
-#   end 
+  def self.genres 
+    @@genres 
+  end 
   
-  def self.artist_count
-  end
+  # def self.artist_count
+  # end
 end #end of class 
