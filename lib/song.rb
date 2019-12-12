e6a3a716299cfa339ea99f9879f352b91b2df46d
Isaac Avilez <isaac1180@aol.com>
2019-12-12 21:21:27 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,21 @@
+require 'pry'
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count +=1
+    @@genres << genre
+    @@artists << artist
+  end
+  
+  def self.count
+    count
+  end
+end #end of class 
