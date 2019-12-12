0b88a9fcf40bc7d76cb3d49fc038e7ac6c0c21a7
Isaac Avilez <isaac1180@aol.com>
2019-12-12 21:33:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,8 +27,7 @@ class Song
     @@genres.uniq
   end 
   
-  def self.genre_count
-    def self.artist_count
+  def self.artist_count
     artist_count = {}
     @@artists.each do |artist|
       if artist_count[artist]
